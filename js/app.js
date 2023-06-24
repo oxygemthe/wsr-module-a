f9f1794fd510ffd3676d61e9e90740d1fabcc2bc
Albert Veyson <veyson@bk.ru>
2023-06-24 05:30:09 +0500

    Главная страница за одну ночь XD

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 window.onload = () => {
-    
+    document.querySelector('header .responsive .open-menu').onclick = () => {
+        document.querySelector('header .responsive .open-menu').classList.toggle('opened')
+        document.querySelector('header .responsive-links').classList.toggle('opened')
+    }
 }
